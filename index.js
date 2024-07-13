8254f3571512ac0e62f67c86546677bd111c96e2
nxvxr14 <hubertdelgado14@gmail.com>
2024-07-13 16:03:21 -0500

    Configurando la base de datos tipo ORM

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express'
 import usuarioRoutes from './routes/usuarioRoutes.js'
+import database from './config/db.js'
 
 // Crea la app
 const app = express();
+
+// Conexion a la base de datos
+try {
+  await database.authenticate();  
+    console.log('Conexion correcta a la base de datos');
+} catch (error) {
+    console.log(error);
+}
 
 // Habilitar PUG
 app.set('view engine', 'pug');

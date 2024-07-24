dfdbc17def3077b852ae82d64759a1eca630027f
nxvxr14 <hubertdelgado14@gmail.com>
2024-07-24 08:51:08 -0500

    Funcionalidad: Editar propiedades

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,6 +1,5 @@
 import { validationResult } from 'express-validator'
 import { Precio, Categoria, Propiedad } from '../models/index.js' 
-import { where } from 'sequelize';
 
 const admin = async (req, res) => {
 
@@ -179,12 +178,120 @@ const almacenarImagen = async (req, res, next) =>{
 }
 
 
+const editarPropiedad = async (req, res) => {
 
+      
+      const {id} = req.params;
+
+      // Validar que la propiedad exista
+      const propiedad = await Propiedad.findByPk(id);
+
+      if(!propiedad){
+            return res.redirect('/propiedades');
+      }
+
+      // Revisa que el id de la propiedad sea la misma del usuario
+      if(propiedad.usuarioId.toString() !== req.usuario.id.toString()){
+            return res.redirect('/propiedades');
+
+      }
+
+      // Consular modelo de precio y categoria
+      const [categorias, precios] = await Promise.all([
+            Categoria.findAll(),
+            Precio.findAll()
+      ]);
+
+      res.render("propiedades/editar", {
+            pagina : 'Editar Propiedad',
+            categorias,
+            precios,
+            datos: propiedad 
+      })
+
+}
+
+const guardarCambiosE = async (req, res) => {
+     
+      // Validacion
+      let resultado = validationResult(req);
+
+      if(!resultado.isEmpty()) {
+      
+
+      // Consular modelo de precio y categoria
+      const [categorias, precios] = await Promise.all([
+            Categoria.findAll(),
+            Precio.findAll()
+      ]);
+
+
+      // Errores
+      return res.render("propiedades/editar", {
+            pagina : 'Editar Propiedad',
+            categorias,
+            precios,
+            errores: resultado.array(),
+            datos: req.body
+      })
+
+    }
+
+      // Validacon con base de datos
+      const {id} = req.params;
+
+      // Validar que la propiedad exista
+      const propiedad = await Propiedad.findByPk(id);
+
+      if(!propiedad){
+            return res.redirect('/propiedades');
+      }
+
+      // Revisa que el id de la propiedad sea la misma del usuario
+      if(propiedad.usuarioId.toString() !== req.usuario.id.toString()){
+            return res.redirect('/propiedades');
+
+      }
+
+      // Reescribir el objeto y actualizarlo
+
+      try {
+
+            const { titulo, descripcion, habitaciones, estacionamiento, wc, calle, lat, lng, precio:precioId, categoria:categoriaId } = req.body
+
+            propiedad.set({
+                  titulo,
+                  descripcion,
+                  habitaciones,
+                  estacionamiento,
+                  wc,
+                  calle,
+                  lat,
+                  lng,
+                  precioId,
+                  categoriaId
+            });
+
+            await propiedad.save();
+
+            res.redirect('/propiedades');
+
+
+      } catch (error) {
+            console.log(error);
+            
+      }
+
+
+
+} 
 
 export {
       admin, 
       crear,
       guardar,
       agregarImagen,
-      almacenarImagen
+      almacenarImagen,
+      editarPropiedad,
+      guardarCambiosE
 }
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { body } from 'express-validator'
-import { admin, crear, guardar, agregarImagen, almacenarImagen } from '../controllers/propiedadController.js'
+import { admin, crear, guardar, agregarImagen, almacenarImagen, editarPropiedad, guardarCambiosE} from '../controllers/propiedadController.js'
 import protegerRuta from '../middleware/protectRoutes.js';
 import upload from '../middleware/subirImagen.js';
 
@@ -34,5 +34,31 @@ router.post(`/propiedades/agregar-imagen/:id`,
     protegerRuta,
     upload.single('imagen'),
     almacenarImagen);
+
+
+router.get('/propiedades/editar/:id',
+    protegerRuta,
+    editarPropiedad
+);
+
+
+router.post('/propiedades/editar/:id',
+    protegerRuta,
+
+    body('titulo').notEmpty().withMessage('El titulo del anuncio es obligatorio.'),
+    body('descripcion')
+    .notEmpty().withMessage('La descripcion es obligatoria.')
+    .isLength({max: 200 }).withMessage('La descripcion es muy larga.'),
+    body('categoria').isNumeric().withMessage('Selecciona una categoria.'),
+    body('habitaciones').isNumeric().withMessage('Selecciona la cantidad de habitaciones.'),
+    body('estacionamiento').isNumeric().withMessage('Selecciona la cantidad de estacionamientos.'),
+    body('wc').isNumeric().withMessage('Selecciona la cantidad de baños.'),
+    body('precio').isNumeric().withMessage('Selecciona un rango de precio.'),
+    body('lat').notEmpty().withMessage('Ubica la propiedad en el mapa.'),
+   guardarCambiosE 
+    
+    
+);
+
 
 export default router
